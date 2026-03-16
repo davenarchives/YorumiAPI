b6390f55fe33948d9f499f5367fe3acef1d4915d
davenarchives <daventheabyss@gmail.com>
2026-03-16 20:57:42 +0800

    chore: remove test scripts

--- test-fetch.js
+++
@@ -1,4 +0,0 @@
-fetch("http://localhost:3001/api/anime/links/naruto-shippuden-355/naruto-shippuden-355?ep=7822")
-  .then(res => res.json())
-  .then(data => console.log(JSON.stringify(data, null, 2)))
-  .catch(console.error);
